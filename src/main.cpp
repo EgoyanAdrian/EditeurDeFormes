b154042b15b7ebb1f91a8823c0a56b243cad3284
Spatium <44459585+Spatiu@users.noreply.github.com>
2019-06-07 22:35:07 +0200

    modif main

--- src/main.cpp
+++ src/main.cpp
@@ -126,10 +126,11 @@ int main() {
 				}
 			}
 		}
-		menuOutils(event,X,Y,isPushLeft,window,gestPoint,gestShape);
-		Menu(screenSize,screenSize,X,Y,isPushLeft,window);//permet la creation du menu
+
 		gestShape.draw(window, X, Y);//coordonne de la a souris X en x et Y en y
 		gestPoint.draw(window, X, Y);
+		menuOutils(event,X,Y,isPushLeft,window,gestPoint,gestShape);
+		Menu(screenSize,screenSize,X,Y,isPushLeft,window);//permet la creation du menu
 		window.display();
 	}
 

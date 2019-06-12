172a52a97c49f8793b50cc31bdd93573c3af1880
J-O-B <39135270+J-O-B@users.noreply.github.com>
2019-06-12 13:14:23 +0200

    main
    main.cpp
    ~ "Renamed gestShape - gestPoint to layoutGestShapes - layoutGestPoints"
    + "isCtrlPressed bool to activate function with key.code and later group shape together"

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(1000, 1000), "Editeur de Formes", sf::Style::Default, settings);
 
 	uint nbS = 0, nbSMax = 10;
-	PointsDrawable gestPoint[nbSMax];
-	ShapesDrawable gestShape[nbSMax];
+	PointsDrawable layoutGestPoints[nbSMax];
+	ShapesDrawable layoutGestShapes[nbSMax];
 
 	bool showLayer = false;
 
@@ -32,6 +32,7 @@ int main() {
 	bool isoverAidee=false;
 	bool ssFich=false;
 	bool ssEdition=false;
+	bool isCtrlPressed=false;
 
 	Point * pointSelect = nullptr;
 	Shape<sf::RenderWindow, edf::Color> * shapeSelect = nullptr;
@@ -65,27 +66,28 @@ int main() {
 					pointSelect->setY(Y - distY);
 				}
 
-				if(shapeSelect != nullptr) {
+				if(shapeSelect != nullptr)
 					shapeSelect->setAnchorXY(X - distX, Y - distY);
-				}
-
 			}
 
-			if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+			if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
 				isPushLeft=true;
-			}
 
 			if(event.type == sf::Event::MouseButtonPressed) {
 				if(showLayer) {
 					uint i = 0;
 					while(pointSelect == nullptr && shapeSelect == nullptr && i < nbSMax) {
-						pointSelect = gestPoint[i].isOver(X, Y);
-						shapeSelect = gestShape[i].isOver(X, Y);
+						pointSelect = layoutGestPoints[i].isOver(X, Y);
+						shapeSelect = layoutGestShapes[i].isOver(X, Y);
 						i++;
 					}
 				} else {
-					pointSelect = gestPoint[nbS].isOver(X, Y);
-					shapeSelect = gestShape[nbS].isOver(X, Y);
+					pointSelect = layoutGestPoints[nbS].isOver(X, Y);
+					shapeSelect = layoutGestShapes[nbS].isOver(X, Y);
+				}
+
+				if(isCtrlPressed) {
+
 				}
 
 				if(pointSelect != nullptr) {
@@ -115,103 +117,118 @@ int main() {
 			}
 
 			if(event.type == sf::Event::KeyPressed) {
-				switch(event.key.code) {
-					case sf::Keyboard::P:
-					 		i = i + 10;
-							gestPoint[nbS].add(new Point(i, 200));
-						break;
-					case sf::Keyboard::R:
-							j = j + 10;
-							gestShape[nbS].add(new RectangleDrawable(j, 280, 10, 20, sf::Color::Black));
-						break;
-					case sf::Keyboard::S:
-							j = j + 10;
-							gestShape[nbS].add(new SquareDrawable(j, 260, 10, sf::Color::Yellow));
-						break;
-					case sf::Keyboard::C:
-							j = j + 10;
-							gestShape[nbS].add(new CircleDrawable(j, 220, 5, sf::Color::Green));
-						break;
-					case sf::Keyboard::E:
-							j = j + 10;
-							gestShape[nbS].add(new EllipseDrawable(j, 240, 10, 5, sf::Color::Black));
-						break;
-					case sf::Keyboard::T:
-							j = j + 10;
-							Point * pointA; pointA = new Point(j, 260);
-							Point * pointB; pointB = new Point(j + 20, 280);
-							Point * pointC; pointC = new Point(j - 5, 300);
-							gestPoint[nbS].add(pointA);
-							gestPoint[nbS].add(pointB);
-							gestPoint[nbS].add(pointC);
-							gestShape[nbS].add(new TriangleDrawable(pointA, pointB, pointC, sf::Color::Green));
-						break;
-					case sf::Keyboard::F:
-							if(shapeSelect != nullptr) {
-								shapeSelect->setFilled(!shapeSelect->getFilled());
-							}
-						break;
-					case sf::Keyboard::Add:
-							if(shapeSelect != nullptr) {
-								shapeSelect->setBorderSize(shapeSelect->getBorderSize() - 1);
-							}
-						break;
-					case sf::Keyboard::Subtract:
-							if(shapeSelect != nullptr) {
-								shapeSelect->setBorderSize(shapeSelect->getBorderSize() + 1);
-							}
-						break;
-					case sf::Keyboard::Delete:
-							if(pointSelect != nullptr) {
-								gestPoint[nbS].remove();
-								pointSelect = nullptr;
-							}
-							if(shapeSelect != nullptr) {
-								gestShape[nbS].remove();
-								shapeSelect = nullptr;
-								lastShapeSelect = nullptr;
-							}
-						break;
-					case sf::Keyboard::Z:
-							if(nbS < (nbSMax - 1))
-								nbS++;
-							else
-								std::cout << "Pas de calques superieurs " << std::endl;
-						break;
-					case sf::Keyboard::X:
-							if(nbS > 0)
-								nbS--;
-							else
-								std::cout << "Pas de calques inferieurs " << nbS << std::endl;
-						break;
-					case sf::Keyboard::A:
-							showLayer = !showLayer;
-						break;
-					case sf::Keyboard::M:
-							saveF.open ("Points.save", std::ofstream::out | std::ofstream::trunc);
-							for (uint i = 0; i < nbSMax; i++)
-								gestPoint[i].save(saveF);
-							saveF.close();
-						break;
-					case sf::Keyboard::N:
-							loadF.open ("Points.save", std::ifstream::in);
-							for (uint i = 0; i < nbSMax; i++)
-								gestPoint[i].load(loadF);
-							loadF.close();
-					default:
-						break;
+				if(isCtrlPressed) {
+					switch(event.key.code) {
+						case sf::Keyboard::P:
+						 		i = i + 10;
+								layoutGestPoints[nbS].add(new Point(i, 200));
+							break;
+						case sf::Keyboard::R:
+								j = j + 10;
+								layoutGestShapes[nbS].add(new RectangleDrawable(j, 280, 10, 20, sf::Color::Black));
+							break;
+						case sf::Keyboard::S:
+								j = j + 10;
+								layoutGestShapes[nbS].add(new SquareDrawable(j, 260, 10, sf::Color::Yellow));
+							break;
+						case sf::Keyboard::C:
+								j = j + 10;
+								layoutGestShapes[nbS].add(new CircleDrawable(j, 220, 5, sf::Color::Green));
+							break;
+						case sf::Keyboard::E:
+								j = j + 10;
+								layoutGestShapes[nbS].add(new EllipseDrawable(j, 240, 10, 5, sf::Color::Black));
+							break;
+						case sf::Keyboard::T:
+								j = j + 10;
+								Point * pointA; pointA = new Point(j, 260);
+								Point * pointB; pointB = new Point(j + 20, 280);
+								Point * pointC; pointC = new Point(j - 5, 300);
+								layoutGestPoints[nbS].add(pointA);
+								layoutGestPoints[nbS].add(pointB);
+								layoutGestPoints[nbS].add(pointC);
+								layoutGestShapes[nbS].add(new TriangleDrawable(pointA, pointB, pointC, sf::Color::Green));
+							break;
+						case sf::Keyboard::Z:
+								if(nbS < (nbSMax - 1))
+									nbS++;
+								else
+									std::cout << "Pas de calques superieurs " << std::endl;
+							break;
+						case sf::Keyboard::X:
+								if(nbS > 0)
+									nbS--;
+								else
+									std::cout << "Pas de calques inferieurs " << nbS << std::endl;
+							break;
+						case sf::Keyboard::A:
+								showLayer = !showLayer;
+							break;
+						case sf::Keyboard::M:
+								saveF.open ("Points.save", std::ofstream::out | std::ofstream::trunc);
+								for (uint i = 0; i < nbSMax; i++)
+									layoutGestPoints[i].save(saveF);
+								saveF.close();
+							break;
+						case sf::Keyboard::N:
+								loadF.open ("Points.save", std::ifstream::in);
+								for (uint i = 0; i < nbSMax; i++)
+									layoutGestPoints[i].load(loadF);
+								loadF.close();
+						default:
+							break;
+					}
+				} else {
+					switch(event.key.code) {
+						case sf::Keyboard::LControl:
+								isCtrlPressed = true;
+							break;
+						case sf::Keyboard::F:
+								if(shapeSelect != nullptr) {
+									shapeSelect->setFilled(!shapeSelect->getFilled());
+								}
+							break;
+						case sf::Keyboard::Add:
+								if(shapeSelect != nullptr) {
+									shapeSelect->setBorderSize(shapeSelect->getBorderSize() - 1);
+								}
+							break;
+						case sf::Keyboard::Subtract:
+								if(shapeSelect != nullptr) {
+									shapeSelect->setBorderSize(shapeSelect->getBorderSize() + 1);
+								}
+							break;
+						case sf::Keyboard::Delete:
+								if(pointSelect != nullptr) {
+									layoutGestPoints[nbS].remove();
+									pointSelect = nullptr;
+								}
+								if(shapeSelect != nullptr) {
+									layoutGestShapes[nbS].remove();
+									shapeSelect = nullptr;
+									lastShapeSelect = nullptr;
+								}
+							break;
+						default:
+							break;
+					}
 				}
+			}
+
+			if(event.type == sf::Event::KeyReleased) {
+				if(event.key.code == sf::Keyboard::LControl)
+					isCtrlPressed = false;
 			}
 		}
 		
 		if(showLayer) {
 			for(uint i = 0; i < 10; i++) {
-				gestShape[i].draw(window, X, Y);
-				gestPoint[i].draw(window, X, Y);
+				layoutGestShapes[i].draw(window, X, Y);
+				layoutGestPoints[i].draw(window, X, Y);
 			}
 		} else {
-			gestShape[nbS].draw(window, X, Y);
-			gestPoint[nbS].draw(window, X, Y);
+			layoutGestShapes[nbS].draw(window, X, Y);
+			layoutGestPoints[nbS].draw(window, X, Y);
 		}
 
 		if(shapeSelect != nullptr) {
@@ -221,7 +238,7 @@ int main() {
 			afficheMenuInfo(font, lastShapeSelect, window.getSize().x,X,Y,isPushLeft, window);
 		}
 
-		Menu(window.getSize().x, window.getSize().y, X, Y, isPushLeft, window,gestPoint,gestShape,showLayer,nbS,nbSMax,isoverFichier,isoverEdition,isoverAidee,ssFich,ssEdition);//permet la creation du menu
+		Menu(window.getSize().x, window.getSize().y, X, Y, isPushLeft, window,layoutGestPoints,layoutGestShapes,showLayer,nbS,nbSMax,isoverFichier,isoverEdition,isoverAidee,ssFich,ssEdition);//permet la creation du menu
 		window.display();
 	}
 
